c067da014bc72cbce873e2cd38f51ba361f195b8
René Sprotte <renspr@users.noreply.github.com>
2023-11-10 14:32:12 +0100

    Always generate HTML and JSON coverage reports.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,12 @@ require "simplecov"
 require "simplecov_json_formatter"
 SimpleCov.start do
   add_filter "/test/"
+  formatter SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCov::Formatter::JSONFormatter
+    ]
+  )
 end
 
 require "minitest/autorun"
